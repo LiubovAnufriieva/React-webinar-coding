2a5a47c2073469d340fcccae1a65fdeee0826c8b
Lyuzika <149685540+LiubovAnufriieva@users.noreply.github.com>
2024-07-25 15:10:18 +0300

    Initial commit

---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,19 @@
+import css from "./FriendList.module.css"
+import { FcOnlineSupport } from "react-icons/fc"
+import { IoCloudOffline } from "react-icons/io5"
+import clsx from "clsx"
+
+
+
+export default function FriendListItem( {friend: { avatar, name, isOnline }}) {
+  return (
+    <div className={css.listItem}>
+      <img className={css.ava} src={avatar} alt="Avatar" width="48" />
+      <p className={css.name}>{name}</p>
+      <p className={clsx(isOnline ? css.online : css.offline)}>
+        {isOnline ? <FcOnlineSupport /> : <IoCloudOffline /> }</p>
+    </div>
+  )
+}
+
+ 
